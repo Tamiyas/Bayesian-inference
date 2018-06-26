a770da23599c31b968ab910da63f42ef94011f14
tmiyaji <miyaji@earth.cs.miyazaki-u.ac.jp>
2018-06-26 16:40:48 +0900

    Add statistics.py

---
+++ statistics.py
@@ -0,0 +1,16 @@
+"""統計量を求めるクラス
+"""
+from data import Data
+
+class Statistics:
+  def __init__(self, data):
+    self.data = data
+
+  def S_G(self):
+    return 1 / h_G()
+
+  def h_G(self):
+    h = 0
+    for x1 in range(0, 2):
+      for x2 in range(0, 2):
+        h +=
