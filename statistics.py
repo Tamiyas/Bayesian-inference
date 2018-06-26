78b9124f0feac4793bccee1c28f04941737310c2
tmiyaji <miyaji@earth.cs.miyazaki-u.ac.jp>
2018-06-26 17:59:27 +0900

    Add S_T

--- statistics.py
+++ statistics.py
@@ -14,17 +14,31 @@ class Statistics:
     h = 0
     for x1 in range(0, 2):
       for x2 in range(0, 2):
-        h += data.Pr(x1, x2) * np.exp(self.N(x1, x2))
+        h += self.data.Pr(x1, x2) * np.exp(self.N(x1, x2))
     return h
 
   def N(self, x1, x2):
     w = 0
     x_vec = [x1, x2]
-    y_vec = data.getYVec()
-    n_list= data.getNList()
+    y_vec = self.data.getYVec()
+    n_list= self.data.getNList()
     for i in range(0, 2):
       for j in range(0, n_list[i]):
-        w += (1 - 2 * y_vec[i][j] + 2 * y_vec[i][j] * x_vec[i] - x_vec[i] ** 2) / data.Pr(1, 1)
+        w += (1 - 2 * y_vec[i][j] + 2 * y_vec[i][j] * x_vec[i] - x_vec[i] ** 2) / self.data.Pr(1, 1)
+    return w
+
+  def S_T(self):
+    return 1 / self.h_T()
+
+  def h_T(self):
+    return 1 + self.data.Pr(0, 0) * np.exp(self.SN()) / self.data.Pr(1, 1)
+
+  def SN(self):
+    y_vec = self.data.getYVec()
+    w = 0
+    for i in range(0, 2):
+      for j in range(0, 3):
+        w += (1 - 2 * y_vec[i][j]) / 2
     return w
 
 if __name__ == '__main__':
@@ -32,3 +46,4 @@ if __name__ == '__main__':
   stat = Statistics(data)
   print(data.getXVec())
   print(stat.S_G())
+  print(stat.S_T())
